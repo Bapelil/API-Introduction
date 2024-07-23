25e97ac182b73081fc63e552984413498dc47a49
Bapelil <141121223+Bapelil@users.noreply.github.com>
2024-07-23 19:56:41 +0200

    Add files via upload

---
+++ Projects/API Introduction/js/config.js
@@ -0,0 +1,2 @@
+const MY_API_KEY = 'cc80795fccmsh1b107073cf4e7f7p11eebdjsn764513f24621';
+export { MY_API_KEY };
---
+++ Projects/API Introduction/js/main.js
@@ -0,0 +1,16 @@
+const url = 'https://quotes15.p.rapidapi.com/quotes/random/?language_code=en';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'cc80795fccmsh1b107073cf4e7f7p11eebdjsn764513f24621',
+		'x-rapidapi-host': 'quotes15.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
